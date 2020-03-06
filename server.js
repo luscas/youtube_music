3ac875b4b7c1c62b21ad69a506b664edc96dd626
Lucas Paz <eu@lucaspaz.com>
2020-03-06 03:33:04 -0300

    4 deploy

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const app = express();
 
 const youtubedl = require("youtube-dl");
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+); // support encoded bodies
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/music", (req, res) => {
   let music = youtubedl(req.query.url, ["-f", "bestaudio[ext=m4a]"], {
     cwd: __dirname
@@ -16,7 +32,7 @@ app.get("/music", (req, res) => {
       thumbnails: info.thumbnails
     });
   });
-});a
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("OK!!!");
